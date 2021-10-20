58fb5d5424f18afdbc6179ef0c513cc76c6de218
Angelica Dilim <angelica.dilim@swapoolabs.com>
2021-10-20 15:24:00 +0800

    Update files

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
-const HtmlWebpackPlugin =  require('html-webpack-plugin');
 const path = require('path');
 
-module.exports  = {
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports  =(env, argv = {})=> {
+
+  const CONFIG = {
     context: __dirname,
     entry: './src/index.js',
-output: {
-    path: path.resolve(__dirname,'dist'),
-    filename: 'main.js',
-    publicPath: '/',
-},
-devServer: {
-      historyApiFallback: true,
-},
-module: {
-    rules: [
-        {
-            test: /\.js$/,
-            use: 'babel-loader'
-        },
-        {
-            test: /\.css?$/,
-            use: ['styles-loader','css-loader']
-        },
-        {
-            test: /\.(png|jpg)?$/,
-            use: 'file-loader?name=./images[name].[ext]'
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'js/[name].bundle.js',
+        publicPath: '/',
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css?$/,
+                use: ['styles-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg)?$/,
+                use: 'file-loader?name=./images[name].[ext]'
+            }
+        ]
+    },
+      
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html' )
         }
-    ]
-},
-   
-plugins: [
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public/index.html' )
-    }
-)]
+    )]
 
+}
+
+return CONFIG
 }
