a1fd0ec084ff4985be07c87a337de7247a43a6f6
Angelica Dilim <angelica.dilim@swapoolabs.com>
2021-10-20 19:18:58 +0800

    Activate sw

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 
 import App from 'components/App';
 
-// import { registerToServicerWorker } from './serviceWorkerRegistration';
+import { registerToServicerWorker } from './serviceWorkerRegistration';
 
 render(<App />, document.getElementById('app'));
+
+registerToServicerWorker();
