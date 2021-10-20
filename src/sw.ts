102bdb51367443c518267953283ed19451e4d6a9
Angelica Dilim <angelica.dilim@swapoolabs.com>
2021-10-21 05:18:04 +0800

    Update sw

--- src/sw.ts
+++ src/sw.ts
@@ -3,8 +3,29 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 declare const self: ServiceWorkerGlobalScope;
 
+// @ts-ignore
+self.addEventListener('install', event => {
+  console.log(caches)
+  console.log(event)
+// @ts-ignore
+  self.skipWaiting(()=> window.location.reload());
 
-skipWaiting();
+
+  // event.waitUntil(
+  //   caches
+  //     .open('my-site-name')
+  //     .then(cache =>
+  //       cache.addAll([
+  //         'favicon.ico',
+  //         'style.css',
+  //         'script.js',
+  //         'https://fonts.googleapis.com/css?family=Inconsolata:400,700'
+  //       ])
+  //     )
+  // )
+})
+
+
 clientsClaim();
 
 // eslint-disable-next-line no-underscore-dangle
