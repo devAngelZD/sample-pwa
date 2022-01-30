3abecb81f79b3407340b97c5819dea8b9f22b078
angelica.dilim <angelica.dilim@swapoolabs.com>
2022-01-30 22:45:34 +0800

    remove skip waiting

--- src/sw.ts
+++ src/sw.ts
@@ -1,11 +1,11 @@
-import { cacheNames, clientsClaim, skipWaiting } from 'workbox-core';
+import { cacheNames } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 
 declare const self: ServiceWorkerGlobalScope & Window & typeof globalThis
 
-self.addEventListener('install', () => {
-  skipWaiting();
-})
+// self.addEventListener('install', () => {
+//   skipWaiting();
+// })
 
 self.addEventListener('install', (event: any) => {
   const urls:any = [/* ... */];
@@ -14,7 +14,7 @@ self.addEventListener('install', (event: any) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
 });
 
-clientsClaim();
+// clientsClaim();
 
 // eslint-disable-next-line no-underscore-dangle
 precacheAndRoute(self.__WB_MANIFEST);
