24f7b1c4dcd991239dc34a0cd46a35aef54c0af5
angelica.dilim <angelica.dilim@swapoolabs.com>
2022-01-31 00:13:21 +0800

    update app.tsx file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const App = () => {
-  return <div>This is a sample pwa site {BUILD_VERSION}
 
-  <div><img src="https://picsum.photos/200/300" alt="text"/></div>
+  return <div>
+    <h1>This is a sample pwa site</h1>
+    <h2>VERSION: {BUILD_VERSION}</h2>
+    <div><img src="https://picsum.photos/200/300" alt="text"/></div>
+    <div id="message"></div>
   </div>;
 
 };
