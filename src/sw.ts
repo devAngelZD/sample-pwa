f94809361be0e11099ecee0bcd1e8c1cbead720c
angelica.dilim <angelica.dilim@swapoolabs.com>
2022-01-31 02:34:44 +0800

    add skipwaiting

--- src/sw.ts
+++ src/sw.ts
@@ -1,19 +1,19 @@
-import { cacheNames, clientsClaim } from 'workbox-core';
+import {  clientsClaim, skipWaiting,  } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 
 declare const self: ServiceWorkerGlobalScope & Window & typeof globalThis
 
-// self.addEventListener('install', () => {
-//   skipWaiting();
-// })
+self.addEventListener('install', () => {
+  skipWaiting();
+})
 
-self.addEventListener('install', (event: any) => {
-  // const urls:any = [/* ... */];
-  const cacheName = cacheNames.runtime;
-  console.log(cacheName)
-  console.log(event)
-  // event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
-});
+// self.addEventListener('install', (event: any) => {
+//   // const urls:any = [/* ... */];
+//   const cacheName = cacheNames.runtime;
+//   console.log(cacheName)
+//   console.log(event)
+//   // event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
+// });
 
 
 clientsClaim();
