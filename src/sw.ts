f383b5934e3324fd06388523fba04c4d65025f6e
angelica.dilim <angelica.dilim@swapoolabs.com>
2022-01-31 08:31:04 +0800

    add log

--- src/sw.ts
+++ src/sw.ts
@@ -2,5 +2,9 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 declare const self: ServiceWorkerGlobalScope & Window & typeof globalThis
 
+self.addEventListener('install', e => {
+  console.log(e)
+})
+
 // eslint-disable-next-line no-underscore-dangle
 precacheAndRoute(self.__WB_MANIFEST);
